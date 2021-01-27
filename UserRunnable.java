ce122942aea7c4060f05bdb7d4d747c5e96d1b53
Alyrii <alyrii0813@gmail.com>
2021-01-27 15:02:28 -0500

    new user commands, implementation pending

--- UserRunnable.java
+++ UserRunnable.java
@@ -1,30 +1,103 @@
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class UserRunnable implements Runnable {
     private Socket socket;
-    public String id;
+    public String name;
     public Room room;
+    private PrintWriter out;
 
     public UserRunnable(Socket s) {
         socket = s;
     }
+
+    public void broadcastMessage(String message) {
+
+    }
     
+    private void sendMessage(String message) {
+
+    }
+
+    private void joinRoom(String id) {
+        
+    }
+
+    private void leaveRoom() {
+
+    }
+
+    private void listRooms() {
+
+    }
+
+    private void listUsers() {
+
+    }
+
+    private void logout() {
+
+    }
+
+    private void setUsername(String name) {
+
+    }
+
+    private void createRoom(String name) {
+
+    }
+
     public void run() {
         try (
             Scanner in = new Scanner(socket.getInputStream());
             PrintWriter out = new PrintWriter(socket.getOutputStream());
         ) {
+            this.out = out;
             while(!Thread.interrupted()) {
                 if(in.hasNextLine()) {
-                    in.nextLine().split(" ");
+                    String input = in.nextLine();
+                    String[] args = input.split(" ");
+                    switch (args[0]) {
+                        case "USERNAME":
+                            setUsername(String.join(" ", Arrays.copyOfRange(args, 1, args.length)));
+                            break;
+                        case "JOIN":
+                            switch (args[1]) {
+                                case "NEW":
+                                    createRoom(String.join(" ", Arrays.copyOfRange(args, 2, args.length)));
+                                    break;
+                                default:
+                                    joinRoom(String.join(" ", Arrays.copyOfRange(args, 1, args.length)));
+                            }
+                            break;
+                        case "LEAVE":
+                            leaveRoom();
+                            break;
+                        case "LOGOUT":
+                            logout();
+                            break;
+                        case "LIST":
+                            switch (args[1]) {
+                                case "ROOMS":
+                                    listRooms();
+                                    break;
+                                case "USERS":
+                                default:
+                                    listUsers();
+                            }
+                            break;
+                        case "MESSAGE":
+                        default:
+                            broadcastMessage(input);
+                    }
                 }
             }
-            
         } catch (Exception e) {
-            System.out.println("Failed to create IO streams.");
+            System.out.println("Failed to create input stream.");
             e.printStackTrace();
         }
     }
+    
 }
